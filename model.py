44bb5c98c63640a9a8f20ac3949650dc155c6a5e
harshi97git <harshu99960@gmail.com>
2025-01-14 23:21:31 +0530

    added 3 new models

---
+++ model.py
@@ -0,0 +1,10 @@
+print("dding all ml related code")
+
+def model1():
+    print("adding code for ml model 1")
+
+def model2():
+    print("adding code for ml model 2")
+
+def model3():
+    print("adding code for ml model 3")
